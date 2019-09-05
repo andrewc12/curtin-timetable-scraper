600676bc6b941043ff3531a5b12555e7ab072854
Andrew Innes <andrew.c12@gmail.com>
2019-09-05 14:04:47 +0800

    pre git import

--- timetable.py
+++ timetable.py
@@ -5,6 +5,17 @@ from selenium.webdriver.firefox.options import Options
 from time import sleep
 import sys
 
+def getallunits(driver):
+    allclass = driver.find_elements_by_xpath("//select[@name='criteriaEntry:allUnits']/option")
+    units = []
+    for elm in allclass:
+        split = elm.text.split(" ", 1)
+        unit = (elm.get_attribute("value"), split[0], split[1] )
+        units.append(unit)
+    print(units)
+    print(units , file=sys.stderr)
+    return units
+
 
 def blankandsearch(driver):
     driver.get("http://timetable.student.curtin.edu.au/criteriaEntry.jsf")
@@ -22,44 +33,32 @@ def blankandsearch(driver):
 
 
 
-def getdata(driver, i):
 
+def viewtimetable(driver, classcode):
     #driver = webdriver.Firefox()
     #driver = webdriver.Firefox(options=options)
     driver.get("http://timetable.student.curtin.edu.au/criteriaEntry.jsf")
     assert "Plan Your Timetable" in driver.title
-    #clear search
-    elem = driver.find_element_by_name("criteriaEntry:filterUnitCodeOrTitle")
-    elem.clear()
-    #elem.send_keys("pycon")
-    #elem.send_keys(Keys.RETURN)
-    #assert "No results found." not in driver.page_source
 
+    #clear all units
     clear = driver.find_element_by_name("criteriaEntry:removeAllButton")
     clear.click()
 
-    #search for all units
-    #srch = driver.find_element_by_name("criteriaEntry:unitSearchButton")
-    #srch.click()
-
-
-
     #select a unit
-    rndclass = driver.find_element_by_xpath("//option[@value=" + i[0] + "]")
+    rndclass = driver.find_element_by_xpath("//option[@value=" + classcode[0] + "]")
     rndclass.click()
 
     #add unit
     add = driver.find_element_by_name("criteriaEntry:addButton")
     add.click()
 
-    #clear all units
-    #clear = driver.find_element_by_name("criteriaEntry:removeAllButton")
-
 
     #view timetable
     view = driver.find_element_by_name("criteriaEntry:timetableForUnitsButton")
     view.click()
 
+
+def getdata(driver):
     #get time table
     classtimetable = driver.find_elements_by_xpath("//table[@class='unitList']/tbody/tr")
     #classtimetable = driver.find_element_by_class_name("unitList")
@@ -76,72 +75,48 @@ def getdata(driver, i):
 
 
 
+def main():
+    options = Options()
+    #options.headless = True
 
-options = Options()
-#options.headless = True
+    driver = webdriver.Firefox(options=options)
 
-#driver = webdriver.Firefox()
-driver = webdriver.Firefox(options=options)
+    blankandsearch(driver)
 
+    units = getallunits(driver) 
 
-
-#get units
-
-#unit table
-#allunits = driver.find_element_by_name("criteriaEntry:allUnits")
-#print(allunits)
-
-blankandsearch(driver)
-
-#get all units as csv
-allclass = driver.find_elements_by_xpath("//select[@name='criteriaEntry:allUnits']/option")
-#print(allclass)
-units = []
-for elm in allclass:
-    split = elm.text.split(" ", 1)
-#    print(elm.get_attribute("value") + "," + split[0] + "," + split[1] )
-    unit = (elm.get_attribute("value"), split[0], split[1] )
-    units.append(unit)
-    #print(unit)
-    #print(elm.text)
-print(units)
-print(units , file=sys.stderr)
-#driver.close()
-
-sleep(5)
+    sleep(5)
 
 
 
 
+    total = len(units) 
+    progress = 1
+
+    for i in units:
+    #    if (progress < 1116):
+    #        progress = progress + 1
+    #        continue
+        for count in range(3):
+            print("grabbing " + str(i[:]) + " " + str(progress) + " out of " + str(total)  , file=sys.stderr)
+            try:
+                viewtimetable(driver, i)
+                getdata(driver)
+                break
+            except:
+                driver.close()
+                sleep(10)
+                driver = webdriver.Firefox(options=options)
+                blankandsearch(driver)
 
 
 
+    #    driver.close()
+        #sleep(3)
+        progress = progress + 1
 
 
+    driver.close()
 
-progress = 1
-total = len(units) 
-
-for i in units:
-#    if (progress < 1116):
-#        progress = progress + 1
-#        continue
-    for count in range(3):
-        print("grabbing " + str(i[:]) + " " + str(progress) + " out of " + str(total)  , file=sys.stderr)
-        try:
-            getdata(driver, i)
-            break
-        except:
-            driver.close()
-            sleep(10)
-            driver = webdriver.Firefox(options=options)
-            blankandsearch(driver)
-
-
-
-#    driver.close()
-    sleep(3)
-    progress = progress + 1
-
-
-driver.close()
+if __name__ == "__main__":
+    main()
