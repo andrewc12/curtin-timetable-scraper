3d4eceb6c4783dddd0aa2d75cb3e736b1387a462
Andrew Innes <andrew.c12@gmail.com>
2019-09-05 14:03:46 +0800

    pre git import

--- timetable.py
+++ timetable.py
@@ -5,63 +5,24 @@ from selenium.webdriver.firefox.options import Options
 from time import sleep
 import sys
 
-options = Options()
-options.headless = True
+
+def blankandsearch(driver):
+    driver.get("http://timetable.student.curtin.edu.au/criteriaEntry.jsf")
+    assert "Plan Your Timetable" in driver.title
+    #clear search
+    elem = driver.find_element_by_name("criteriaEntry:filterUnitCodeOrTitle")
+    elem.clear()
+    #elem.send_keys("pycon")
+    #elem.send_keys(Keys.RETURN)
+    #assert "No results found." not in driver.page_source
+    
+    #search for all units
+    srch = driver.find_element_by_name("criteriaEntry:unitSearchButton")
+    srch.click()
 
 
 
-
-#get units
-#driver = webdriver.Firefox()
-driver = webdriver.Firefox(options=options)
-driver.get("http://timetable.student.curtin.edu.au/criteriaEntry.jsf")
-assert "Plan Your Timetable" in driver.title
-#clear search
-elem = driver.find_element_by_name("criteriaEntry:filterUnitCodeOrTitle")
-elem.clear()
-#elem.send_keys("pycon")
-#elem.send_keys(Keys.RETURN)
-#assert "No results found." not in driver.page_source
-
-#search for all units
-srch = driver.find_element_by_name("criteriaEntry:unitSearchButton")
-srch.click()
-
-#unit table
-#allunits = driver.find_element_by_name("criteriaEntry:allUnits")
-#print(allunits)
-
-
-
-#get all units as csv
-allclass = driver.find_elements_by_xpath("//select[@name='criteriaEntry:allUnits']/option")
-#print(allclass)
-units = []
-for elm in allclass:
-    split = elm.text.split(" ", 1)
-#    print(elm.get_attribute("value") + "," + split[0] + "," + split[1] )
-    unit = (elm.get_attribute("value"), split[0], split[1] )
-    units.append(unit)
-    #print(unit)
-    #print(elm.text)
-print(units)
-print(units , file=sys.stderr)
-#driver.close()
-
-sleep(5)
-
-
-
-
-
-
-
-progress = 1
-total = len(units) 
-
-for i in units:
-    print("grabbing " + str(i[:]) + " " + str(progress) + " out of " + str(total)  , file=sys.stderr)
-
+def getdata(driver, i):
 
     #driver = webdriver.Firefox()
     #driver = webdriver.Firefox(options=options)
@@ -99,14 +60,6 @@ for i in units:
     view = driver.find_element_by_name("criteriaEntry:timetableForUnitsButton")
     view.click()
 
-
-
-
-
-
-
-
-
     #get time table
     classtimetable = driver.find_elements_by_xpath("//table[@class='unitList']/tbody/tr")
     #classtimetable = driver.find_element_by_class_name("unitList")
@@ -122,10 +75,72 @@ for i in units:
 
 
 
-    sleep(1)
+
+
+options = Options()
+#options.headless = True
+
+#driver = webdriver.Firefox()
+driver = webdriver.Firefox(options=options)
+
+
+
+#get units
+
+#unit table
+#allunits = driver.find_element_by_name("criteriaEntry:allUnits")
+#print(allunits)
+
+blankandsearch(driver)
+
+#get all units as csv
+allclass = driver.find_elements_by_xpath("//select[@name='criteriaEntry:allUnits']/option")
+#print(allclass)
+units = []
+for elm in allclass:
+    split = elm.text.split(" ", 1)
+#    print(elm.get_attribute("value") + "," + split[0] + "," + split[1] )
+    unit = (elm.get_attribute("value"), split[0], split[1] )
+    units.append(unit)
+    #print(unit)
+    #print(elm.text)
+print(units)
+print(units , file=sys.stderr)
+#driver.close()
+
+sleep(5)
+
+
+
+
+
+
+
+
+
+
+progress = 1
+total = len(units) 
+
+for i in units:
+#    if (progress < 1116):
+#        progress = progress + 1
+#        continue
+    for count in range(3):
+        print("grabbing " + str(i[:]) + " " + str(progress) + " out of " + str(total)  , file=sys.stderr)
+        try:
+            getdata(driver, i)
+            break
+        except:
+            driver.close()
+            sleep(10)
+            driver = webdriver.Firefox(options=options)
+            blankandsearch(driver)
+
+
 
 #    driver.close()
-
+    sleep(3)
     progress = progress + 1
 
 
