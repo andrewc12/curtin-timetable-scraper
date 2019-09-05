d964bd1fb134bfb1692ac1cbe74312c0f926d8f7
Andrew Innes <andrew.c12@gmail.com>
2019-09-05 14:02:50 +0800

    pre git import

--- timetable.py
+++ timetable.py
@@ -1,11 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+
 from time import sleep
+import sys
+
+options = Options()
+options.headless = True
 
 
 
 
-driver = webdriver.Firefox()
+#get units
+#driver = webdriver.Firefox()
+driver = webdriver.Firefox(options=options)
 driver.get("http://timetable.student.curtin.edu.au/criteriaEntry.jsf")
 assert "Plan Your Timetable" in driver.title
 #clear search
@@ -37,49 +45,82 @@ for elm in allclass:
     #print(unit)
     #print(elm.text)
 print(units)
+print(units , file=sys.stderr)
 driver.close()
 
-
-
-
-
-#select a unit
-rndclass = driver.find_element_by_xpath("//option[@value='315560']")
-rndclass.click()
-
-#add unit
-add = driver.find_element_by_name("criteriaEntry:addButton")
-add.click()
-
-#clear all units
-clear = driver.find_element_by_name("criteriaEntry:removeAllButton")
-
-
-#view timetable
-view = driver.find_element_by_name("criteriaEntry:timetableForUnitsButton")
-view.click()
+sleep(5)
 
 
 
 
 
-sleep(1)
+
+
+progress = 1
+total = len(units) 
+
+for i in units:
+    print("grabbing " + str(i[:]) + " " + str(progress) + " out of " + str(total)  , file=sys.stderr)
+
+
+    #driver = webdriver.Firefox()
+    driver = webdriver.Firefox(options=options)
+    driver.get("http://timetable.student.curtin.edu.au/criteriaEntry.jsf")
+    assert "Plan Your Timetable" in driver.title
+    #clear search
+    elem = driver.find_element_by_name("criteriaEntry:filterUnitCodeOrTitle")
+    elem.clear()
+    #elem.send_keys("pycon")
+    #elem.send_keys(Keys.RETURN)
+    #assert "No results found." not in driver.page_source
+
+    #search for all units
+    srch = driver.find_element_by_name("criteriaEntry:unitSearchButton")
+    srch.click()
+
+
+
+    #select a unit
+    rndclass = driver.find_element_by_xpath("//option[@value=" + i[0] + "]")
+    rndclass.click()
+
+    #add unit
+    add = driver.find_element_by_name("criteriaEntry:addButton")
+    add.click()
+
+    #clear all units
+    clear = driver.find_element_by_name("criteriaEntry:removeAllButton")
+
+
+    #view timetable
+    view = driver.find_element_by_name("criteriaEntry:timetableForUnitsButton")
+    view.click()
 
 
 
 
-#get time table
-classtimetable = driver.find_elements_by_xpath("//table[@class='unitList']/tbody/tr")
-#classtimetable = driver.find_element_by_class_name("unitList")
-#print(classtimetable.text)
-for i in classtimetable:
-    print(i.text)
-    #for j in i.find_elements_by_tag_name("td"):
-    #    print(j.text)
-    comp_list = [j.text for j in i.find_elements_by_tag_name("td")]
-    print(comp_list)
 
 
 
 
 
+    #get time table
+    classtimetable = driver.find_elements_by_xpath("//table[@class='unitList']/tbody/tr")
+    #classtimetable = driver.find_element_by_class_name("unitList")
+    #print(classtimetable.text)
+    for i in classtimetable:
+        #print(i.text)
+        #for j in i.find_elements_by_tag_name("td"):
+        #    print(j.text)
+        comp_list = [j.text for j in i.find_elements_by_tag_name("td")]
+        print(comp_list)
+        print(comp_list  , file=sys.stderr)
+
+
+
+
+    sleep(1)
+
+    driver.close()
+
+    progress = progress + 1
